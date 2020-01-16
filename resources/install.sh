1ee638d5e12d28f722181d36ca097b95c54e661b
totoff974 <totoff974@users.noreply.github.com>
2020-01-16 23:52:41 +0400

    Create install.sh

---
+++ resources/install.sh
@@ -0,0 +1,13 @@
+touch /tmp/dependancy_JeeMySensors_in_progress
+echo 0 > /tmp/dependancy_JeeMySensors_in_progress
+echo "********************************************************"
+echo "*             Installation des dépendances             *"
+echo "********************************************************"
+apt-get update
+echo 50 > /tmp/dependancy_JeeMySensors_in_progress
+apt-get install -y python-serial python-requests python-pyudev
+echo 100 > /tmp/dependancy_JeeMySensors_in_progress
+echo "********************************************************"
+echo "*             Installation terminée                    *"
+echo "********************************************************"
+rm /tmp/dependancy_JeeMySensors_in_progress
