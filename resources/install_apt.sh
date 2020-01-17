ee11013b78ddd04a089733017ba7e4f03a6b14ca
totoff974 <totoff974@users.noreply.github.com>
2020-01-17 16:32:02 +0400

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,22 +1,15 @@
 PROGRESS_FILE=/tmp/dependancy_JeeMySensors_in_progress
+if [ ! -z $1 ]; then
+	PROGRESS_FILE=$1
+fi
 touch ${PROGRESS_FILE}
 echo 0 > ${PROGRESS_FILE}
-
-function apt_install {
-  sudo apt-get -y install "$@"
-  if [ $? -ne 0 ]; then
-    echo "could not install $1 - abort"
-    rm ${PROGRESS_FILE}
-    exit 1
-  fi
-}
-
 echo "********************************************************"
 echo "*             Installation des dépendances             *"
 echo "********************************************************"
-sudo apt-get update
+apt-get update
 echo 50 > ${PROGRESS_FILE}
-apt_install python-serial python-requests python-pyudev
+apt-get install -y python-serial python-requests python-pyudev
 echo 100 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation terminée                    *"
